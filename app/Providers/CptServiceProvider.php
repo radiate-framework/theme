b8103e98662aeffedbbb2bd17296e24f7000c094
Ben Rutland <benrutland@hotmail.com>
2021-02-16 22:50:16 +0000

    added cpt provider

---
+++ app/Providers/CptServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Theme\Providers;
+
+use Radiate\Foundation\Providers\CptServiceProvider as ServiceProvider;
+
+class CptServiceProvider extends ServiceProvider
+{
+    /**
+     * The custom post types and taxonomies to register
+     *
+     * @var array
+     */
+    protected $postTypes = [
+        //
+    ];
+}
